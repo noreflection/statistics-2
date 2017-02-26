42a1b55234bf941c33a55ef13170ccf4fa58bac6
noreflection <a191a191a@gmail.com>
2017-02-26 11:55:20 +0300

    test page, libraries update

--- Statistics/Controllers/HomeController.cs
+++ Statistics/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Statistics.Models;
 
 namespace Statistics.Controllers
 {
@@ -11,6 +12,16 @@ namespace Statistics.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Saved(Item item)
+        {
+            return View(item);
         }
 
         public IActionResult About()
---
+++ Statistics/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statistics.Models
+{
+    public class Item
+    {
+        [Required(ErrorMessage = "Add item name")]
+        public string ItemName { get; set; }
+
+        [Required(ErrorMessage = "Add item description")]
+        public string ItemDescription { get; set; }
+    }
+}
